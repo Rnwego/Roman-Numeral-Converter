b608cd74b3913b8725addc0415d98097cd3ff659
Ruth Nwego <ruthnwego@gmail.com>
2021-07-29 20:32:46 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function convertToRoman(num) {
+    if (isNaN(num)) return NaN;
+    var romanNumerals = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+    var roman = '';
+    var i;
+    for (i in romanNumerals) {
+        while (num >= romanNumerals[i]) {
+            roman += i;
+            num -= romanNumerals[i];
+        }
+    }
+    return roman;
+}
+
+convertToRoman(36);
